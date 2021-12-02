52332824ff85c2a3fde6d4743788a99213e93696
loic.saillant <loic.saillant.dev@gmail.com>
2021-12-02 13:21:50 +0100

    Initial Commit

---
+++ 10000days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10000days
+//
+//  Created by Loïc Saillant on 02/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
